c72ec2cf6d20560d2ceb47118703469441040604
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-06-24 13:43:51 -0300

    login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,21 @@
+const Usuario = require("../models/Usuario");
+
 async function abreTela(req, res) {
   res.render("login/login.ejs");
 }
 
 async function logar(req, res) {}
 
-async function cadastro(req, res) {}
+async function cadastro(req, res) {
+  var nome = req.body.nome;
+  var email = req.body.email;
+  var senha = req.body.senha;
+  const usuario = await Usuario.create({
+    nome: nome,
+    email: email,
+    senha: senha,
+  });
+  res.redirect("/");
+}
 
 module.exports = { abreTela, logar, cadastro };
--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const loginRoute = require("./routes/loginRoute");
 //configuração dos arquivos de visão (VIEWS)
 app.set("view engine", "ejs");
 
+//configurar para receber dados por metodo post
+app.use(express.urlencoded({ extended: false }));
+
 //pasta de arquivos estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
