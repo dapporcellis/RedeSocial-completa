c9217fa9fb5e678deb45dd6e960e2976214577f6
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-07-12 13:47:28 -0300

    views completas

--- controllers/principalController.js
+++ controllers/principalController.js
@@ -2,4 +2,46 @@ async function abregaleria(req, res) {
   res.render("principal/galeria");
 }
 
-module.exports = { abregaleria };
+async function postarfoto(req, res) {
+  res.render("principal/postarfoto");
+}
+
+async function postagem(req, res) {
+  res.render("principal/postagem");
+}
+
+async function postagens(req, res) {
+  res.render("principal/postagens");
+}
+
+async function listaramigos(req, res) {
+  res.render("principal/listaramigos");
+}
+
+async function buscaramigos(req, res) {
+  res.render("principal/buscaramigos");
+}
+
+async function buscarcomunidade(req, res) {
+  res.render("principal/buscarcomunidade");
+}
+
+async function minhascomunidades(req, res) {
+  res.render("principal/minhascomunidades");
+}
+
+async function criarcomunidade(req, res) {
+  res.render("principal/criarcomunidade");
+}
+
+module.exports = {
+  abregaleria,
+  postarfoto,
+  postagens,
+  listaramigos,
+  postagem,
+  buscaramigos,
+  buscarcomunidade,
+  minhascomunidades,
+  criarcomunidade,
+};
--- routes/principalRoute.js
+++ routes/principalRoute.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const principalController = require("../controllers/principalController");
-
+const upload = require("../config/upload");
 router.get("/galeria", principalController.abregaleria);
+router.get("/postarfotos", principalController.postarfoto);
+router.get("/postagem", principalController.postagem);
+router.get("/postagens", principalController.postagens);
+router.get("/listaramigos", principalController.listaramigos);
+router.get("/buscaramigos", principalController.buscaramigos);
+router.get("/buscarcomunidade", principalController.buscarcomunidade);
+router.get("/minhascomunidades", principalController.minhascomunidades);
+router.get("/criarcomunidade", principalController.criarcomunidade);
+
+router.post("/postarfotos", upload.single("foto"), function (req, res) {
+  console.log(req.file);
+});
 
 module.exports = router;
