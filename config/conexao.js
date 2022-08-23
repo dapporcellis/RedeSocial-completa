044f4416dd6ebef55f71f1846bc2c76547123fd5
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-08-23 14:49:54 -0300

    heroku

--- config/conexao.js
+++ config/conexao.js
@@ -1,9 +1,14 @@
 const sequelize = require("sequelize");
 
-const conexao = new sequelize("redesocial_teste", "postgres", "postgres", {
-  host: "localhost",
-  port: "5433",
-  dialect: "postgres",
-});
+const conexao = new sequelize(
+  "d19pntvbo46qgm",
+  "tawhihlqjlofhk",
+  "fa9f85c9d7116ebee41d93095964ceb247ac9b978ed813814d34ed07c37d79ca",
+  {
+    host: "ec2-34-235-198-25.compute-1.amazonaws.com",
+    port: "5432",
+    dialect: "postgres",
+  }
+);
 
 module.exports = conexao;
